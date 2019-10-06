fad4e18e56a5b8a279c6922264d03dd42f5b5ff5
ratsuda suwan <ratsuda.su@ku.th>
2019-10-06 19:09:15 +0700

    add feature and step definition

--- src/test/java/ku/atm/StepDefATM.java
+++ src/test/java/ku/atm/StepDefATM.java
@@ -49,6 +49,11 @@ public class StepDefATM {
         atm.withdraw(amount);
     }
 
+    @When("I deposit (.*) from ATM")
+    public void i_deposit_from_atm(double money) {
+        atm.deposit(money);
+    }
+
     @When("I overdraw (.*) from ATM")
     public void i_withdraw_from_atm_more_than_balance(double amount) throws NotEnoughBalanceException {
         assertThrows(NotEnoughBalanceException.class,
@@ -67,7 +72,7 @@ public class StepDefATM {
     @Then("customer id (\\d+) account balance is (.*)")
     public void customer_id_account_balance_is(int id, double balance) {
         assertEquals(balance,
-                     bank.findCustomer(id).getAccount().getBalance());
+                bank.findCustomer(id).getAccount().getBalance());
     }
 
 }
